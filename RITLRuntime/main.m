117923d925dd6355b79687ed863fecbcb8421209
774655048@qq.com <774655048@qq.com>
2016-10-27 13:38:56 +0800

    Initial Commit

---
+++ RITLRuntime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RITLRuntime
+//
+//  Created by YueWen on 2016/10/27.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
