fcab1cd4eef5f99c45326f543c3ff3f0daa2e2e6
774655048@qq.com <774655048@qq.com>
2016-10-27 14:19:13 +0800

    add RILTPeople class in RITLRuntimeIvarsOrPropertys

---
+++ RITLRuntimeIvalsOrPropertys/RITLPeople.h
@@ -0,0 +1,13 @@
+//
+//  RITLPeople.h
+//  RITLRuntime
+//
+//  Created by YueWen on 2016/10/27.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RITLPeople : NSObject
+
+@end
---
+++ RITLRuntimeIvalsOrPropertys/RITLPeople.m
@@ -0,0 +1,13 @@
+//
+//  RITLPeople.m
+//  RITLRuntime
+//
+//  Created by YueWen on 2016/10/27.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import "RITLPeople.h"
+
+@implementation RITLPeople
+
+@end
